79085ed001c02c791baba9482abf9049d918e1bf
Jakub Zalewski <zalew7@gmail.com>
2012-06-27 05:12:22 +0200

    photo templatetag fallback for old photos that don't have a large size (according to flickr spec). size dimensions don't work.

--- flickr/templatetags/flickr_tags.py
+++ flickr/templatetags/flickr_tags.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from django import template
+import datetime
 register = template.Library()
 
 
 @register.inclusion_tag("flickr/photo.html")
 def flickr_photo(photo, size='medium', flickr_link=False):
+    if size == 'large' and photo.date_posted <= datetime.datetime(2010, 05, 25):
+        if photo.user.ispro:
+            size = 'ori'
+        else:
+            size = 'medium640'
     return {'photo': photo, 'size': size, 'flickr_link': flickr_link, }
