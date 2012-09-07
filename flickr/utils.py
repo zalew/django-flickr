25278f6e2270637d5c9c1eacebad425315323ab5
Javier García Sogo <jgsogo@gmail.com>
2012-09-07 13:04:28 +0200

    time awareness based on user offset

--- flickr/utils.py
+++ flickr/utils.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 
-def ts_to_dt(timestamp):
-    return datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+def ts_to_dt(timestamp, offset=''):
+    return '%s%s' % (datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'), offset)
 
 
 def unslash(url):
